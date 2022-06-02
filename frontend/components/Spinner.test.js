cffec3cb26d949e0da64d0e3f913eb929963aa6f
Paul Morrow <pmorow93@gmail.com>
2022-06-01 23:42:53 -0500

    Tests passing

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,15 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import Spinner from "./Spinner";
+import { render } from "@testing-library/react";
+import React from "react";
+
+test("renders the spinner-if true renders component", () => {
+  const { container } = render(<Spinner on={true} />);
+  expect(container.firstChild).toBeTruthy();
+});
+
+test("renders the spinner-if false renders null", () => {
+  const { container } = render(<Spinner on={false} />);
+  expect(container.firstChild).toBe(null);
+});
